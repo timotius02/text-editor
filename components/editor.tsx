b141752e0738c7df625cd43755315b8feb1c1bf3
Timotius Sitorus <timsitorus1@gmail.com>
2022-12-03 15:36:45 +0700

    init

---
+++ components/editor.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
+
+const initialValue = [
+  {
+    type: "paragraph" as const,
+    children: [{ text: "A line of text in a paragraph." }],
+  },
+];
+
+export default function Counter() {
+  const [editor] = useState(() => withReact(createEditor()));
+
+  return (
+    <Slate editor={editor} value={initialValue}>
+      <Editable />
+    </Slate>
+  );
+}
